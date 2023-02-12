422eccfc9a3b71a55017024051a45d5588e28bed
vagap <you@example.com>
2023-02-12 16:19:28 +0300

    task

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array1 = new string[4] {"12003", "23", "hello", "world"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array1);
+PrintArray(array2);
